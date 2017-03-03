9bff7e6699eca151febe8dbbe02a770ee5bb9102
rekha014 <rekhdhakad014@gmail.com>
2017-03-03 17:02:52 +0530

    added code

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,38 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def testimonials
+  end
+
+  def portfolio
+  end
+
+  def contact_us
+  end
+
+  def careers
+  end 
+
+  def team
+  end 
+
+  def matrimonial
+  end 
+
+  def shine_brand
+  end 
+
+  def legal_properties
+  end
+  
+  def it_services
+  end 
+
+  def import_export
+  end 
+
+
+  def contruction
+  end 
+ end 
